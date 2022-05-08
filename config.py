feb8a528cf8496e1beb3c2de19a6f5d99a4077bd
Tanmay Thole <tanmaythole@gmail.com>
2022-05-08 23:54:40 +0530

    User, projects table added in postgres

--- config.py
+++ config.py
@@ -7,6 +7,12 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
 
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
 
 # Secret Key
 SECRET_KEY = os.getenv("SECRET_KEY")

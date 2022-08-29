913f27d5d5208fc478312dd374420f712e945b52
Tanmay Thole <tanmaythole@gmail.com>
2022-08-29 16:52:32 +0530

    Add Schedule bug fixed

--- app/routes/scheduler.py
+++ app/routes/scheduler.py
@@ -41,6 +41,7 @@ def addScheduledJob():
     with app.app_context():
         try:
             req_data = request.json
+            print(req_data)
             req_data['project'] = get_project_id(req_data.get('project'))
             user = get_current_user()
             req_data['scheduled_by'] = user.id
@@ -76,6 +77,7 @@ def addScheduledJob():
             else:
                 return jsonify({"Error" : "You do not have access to this project, kindly connect to project admin to schedule testsuites of the projects"}),401
         except Exception as e:
+            print(e)
             return jsonify(str(e) + "----------"),400
 
 def to_frequency(frequency_type,custom_frequency):

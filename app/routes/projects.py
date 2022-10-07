d2966b6fa62ec306f287f9bf1a293864c085f550
poojan-2601 <poojannaik88@gmail.com>
2022-10-07 11:43:26 +0530

    reworked if else block

--- app/routes/projects.py
+++ app/routes/projects.py
@@ -61,10 +61,8 @@ def createProjects():
 
         super_admin = UserModel.query.filter_by(is_super_admin=True).first()
         project = ProjectModel(data)
-        if user.id == super_admin.id:
-            project.project_members.append(user)
-        else:
-            project.project_members.append(user)
+        project.project_members.append(user)
+        if user.id != super_admin.id:
             project.project_members.append(super_admin)
         project.save()
         return jsonify({"success": "project created successfully"}),200

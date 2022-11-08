b151cc0589d595fe520b39509b4af9d3a5bdfb20
poojan naik <poojannaik88@gmail.com>
2022-11-08 15:03:18 +0530

    engine modification for testsuite execution

--- app/routes/engine.py
+++ app/routes/engine.py
@@ -2,23 +2,57 @@ from datetime import datetime
 import requests
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required
-from app.helpers.utils import is_fit_to_run,get_current_user
+from app.helpers.utils import is_fit_to_run, get_current_user
 from app.models.EnvModel import EnvModel
 from app.models.ResultModel import ResultModel
 from app.models.TempModel import TempModel
 from app.models.TestcaseModel import TestcaseModel
+from app.models.TestsuiteModel import TestsuiteModel
 
 engine_blueprint = Blueprint('engine', __name__)
 
+
 @engine_blueprint.route('/api/tests', methods=['POST'])
 @jwt_required()
-def tests():
+def engine():
     try:
-        data = request.json
+        req_data = request.json
         user = get_current_user().id
-        testcase = TestcaseModel.get_one_testcase(data.get('testcase'))
-        environment = EnvModel.get_one_env(data['environment'])
+        
+        if not ((req_data.get('testsuite') and req_data.get('level') == 'testsuite') or (req_data.get('testcase') and req_data.get('level') == 'testcase')) and req_data.get('environment'):
+            return jsonify({"error": "incomplete request, kindly send all required parameters"}), 400
 
+        execution_data = {'user' : user}
+        if req_data.get('level') == "testsuite":
+            testsuite = TestsuiteModel.get_one_testsuite(id=req_data['testsuite'])
+            result = []
+            no_of_passed_testcases = 0
+            no_of_failed_testcases = 0
+            for testcase in testsuite['testcases']:
+                execution_data['environment'] = EnvModel.get_one_env(req_data['environment'])
+                execution_data['testcase'] = testcase
+                # result.append({testcase['name']:tests(data=execution_data)})
+                resp = tests(data=execution_data)
+            return jsonify({"result" : result}), 200
+        else:
+            execution_data['environment'] = EnvModel.get_one_env(req_data['environment'])
+            execution_data['testcase'] = TestcaseModel.get_one_testcase(req_data['testcase'])
+            response = tests(data=execution_data)
+
+        if response.get('error') is not None:
+            return jsonify({"error": response['error']}), 400
+
+        return jsonify({"result": response['result']}), 200
+    except Exception as err:
+        print(str(err))
+        return jsonify({"error" : "something went wrong"})
+
+
+def tests(data):
+    try:
+        testcase = data['testcase']
+        environment = data['environment']
+        user = data['user']
         is_fit, missing_components = is_fit_to_run(testcase)
         if is_fit:
             res = []
@@ -112,11 +146,12 @@ def tests():
             }
             ResultModel(data_to_store).save()
             TempModel.get_all_and_delete(testcase=testcase['id'],run_time_id=unique_run_time_id)
-            return jsonify({"result": res}), 200
+            return {"result": res}
         else:
-            return jsonify({"error" : missing_components}),400
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
+            return {"error": missing_components}
+    except Exception as err:
+        # print(str(err))
+        return {"error": str(err)}
 
 
 def fetch_from_api(teststep):
@@ -126,31 +161,38 @@ def fetch_from_api(teststep):
     # resp = s.send(prepped)
     # return resp
     try:
-        if teststep['method'].lower()=='get':
-            r = requests.get(url=teststep['endpoint'], json=teststep['payload'], headers=teststep['header'], params=teststep['parameters'])
-        elif teststep['method'].lower()=='post':
-            r = requests.post(url=teststep['endpoint'], json=teststep['payload'], headers=teststep['header'], params=teststep['parameters'])
-        elif teststep['method'].lower()=='put':
-            r = requests.put(url=teststep['endpoint'], json=teststep['payload'], headers=teststep['header'], params=teststep['parameters'])
-        elif teststep['method'].lower()=='patch':
-            r = requests.patch(url=teststep['endpoint'], json=teststep['payload'], headers=teststep['header'], params=teststep['parameters'])
-        elif teststep['method'].lower()=='delete':
-            r = requests.delete(url=teststep['endpoint'], json=teststep['payload'], headers=teststep['header'], params=teststep['parameters'])
+        if teststep['method'].lower() == 'get':
+            r = requests.get(url=teststep['endpoint'], json=teststep['payload'],
+                             headers=teststep['header'], params=teststep['parameters'])
+        elif teststep['method'].lower() == 'post':
+            r = requests.post(url=teststep['endpoint'], json=teststep['payload'],
+                              headers=teststep['header'], params=teststep['parameters'])
+        elif teststep['method'].lower() == 'put':
+            r = requests.put(url=teststep['endpoint'], json=teststep['payload'],
+                             headers=teststep['header'], params=teststep['parameters'])
+        elif teststep['method'].lower() == 'patch':
+            r = requests.patch(url=teststep['endpoint'], json=teststep['payload'],
+                               headers=teststep['header'], params=teststep['parameters'])
+        elif teststep['method'].lower() == 'delete':
+            r = requests.delete(url=teststep['endpoint'], json=teststep['payload'],
+                                headers=teststep['header'], params=teststep['parameters'])
         return r
     except Exception as err:
         return str(err)
 
-def perform_teststeps(teststep, testcase, user, environment,unique_run_time_id):
-    
+
+def perform_teststeps(teststep, testcase, user, environment, unique_run_time_id):
+
     if "$var=" in str(teststep):
-        pattern =  "\$var\=(.*?)\'"
+        pattern = "\$var\=(.*?)\'"
         import re
         variable = re.search(pattern, str(teststep)).group(1)
         tmp = variable.split('.')
-        var_value = TempModel.get_one(testcase=testcase['id'], teststeps=tmp[0],run_time_id=unique_run_time_id)
-        
+        var_value = TempModel.get_one(
+            testcase=testcase['id'], teststeps=tmp[0], run_time_id=unique_run_time_id)
+
         if var_value is None:
-            outcome  = [
+            outcome = [
                 {
                     "res_value": "Not found",
                     "executed_status": "failed",
@@ -159,51 +201,53 @@ def perform_teststeps(teststep, testcase, user, environment,unique_run_time_id):
                 }
             ]
             return {
-                "status" : "failed",
-                "outcome" : outcome,
-                "response" : {"Error" : "Testdata does not exist hence execution aborted"},
+                "status": "failed",
+                "outcome": outcome,
+                "response": {"Error": "Testdata does not exist hence execution aborted"},
                 "no_of_passed_fields": 0,
                 "no_of_failed_fields": 0
             }
 
         for i in tmp[1:len(tmp)]:
             var_value = var_value.get(i)
-            
         teststep = eval(str(teststep).replace(f"$var={variable}", var_value))
-            
+
     res = fetch_from_api(teststep)
     if type(res) is str:
-        outcome  = [
-                {
-                    "res_value": "Not found",
-                    "executed_status": "failed",
-                    "status": "failed",
-                    "error": res
-                }
-            ]
-        return {
-                "status" : "failed",
-                "outcome" : outcome,
-                "response" : {"Error" : "Testdata does not exist hence execution aborted"},
-                "no_of_passed_fields": 0,
-                "no_of_failed_fields": 0
+        outcome = [
+            {
+                "res_value": "Not found",
+                "executed_status": "failed",
+                "status": "failed",
+                "error": res
             }
-    temp = TempModel({"testcase": testcase['id'], "teststep": teststep['name'], "resp": res.json(), "run_time_id" : unique_run_time_id})
+        ]
+        return {
+            "status": "failed",
+            "outcome": outcome,
+            "response": {"Error": "Testdata does not exist hence execution aborted"},
+            "no_of_passed_fields": 0,
+            "no_of_failed_fields": 0
+        }
+    temp = TempModel({"testcase": testcase['id'], "teststep": teststep['name'], "resp": res.json(
+    ), "run_time_id": unique_run_time_id})
     temp.save()
-    
-    status, outcome, no_of_passed_fields, no_of_failed_fields = validate_expected_outcome(teststep, res)
+
+    status, outcome, no_of_passed_fields, no_of_failed_fields = validate_expected_outcome(
+        teststep, res)
 
     return {
-        "status": "passed" if status=="passed" else "failed",
+        "status": "passed" if status == "passed" else "failed",
         "outcome": outcome,
         "response": res.json(),
         "no_of_passed_fields": no_of_passed_fields,
         "no_of_failed_fields": no_of_failed_fields
     }
 
+
 def validate_expected_outcome(teststep, response):
     """
-    
+
     """
 
     no_of_passed_fields = 0
@@ -225,16 +269,16 @@ def validate_expected_outcome(teststep, response):
         else:
             res_value = res.get(field_name)
             if not res_value:
-                is_failed =  True
+                is_failed = True
                 error = f"Expected Field not found in response."
             else:
                 if field['isExact']:
-                    if field.get('value')!=res_value:
+                    if field.get('value') != res_value:
                         is_failed = True
                         error = f"Response value is not matched with Expected value."
                 elif field.get('validations'):
                     validations = field.get('validations')
-                    
+
                     max_length = validations.get('maxLength')
                     min_length = validations.get('minLength')
                     max_value = validations.get('maxValue')
@@ -242,13 +286,13 @@ def validate_expected_outcome(teststep, response):
                     regex_pattern = validations.get('regex')
 
                     err = []
-                    if max_length and len(res_value)>int(max_length):
+                    if max_length and len(res_value) > int(max_length):
                         err.append('maxLength')
-                    if min_length and len(res_value)<int(min_length):
+                    if min_length and len(res_value) < int(min_length):
                         err.append('minLength')
-                    if min_value and int(res_value)<int(min_value):
+                    if min_value and int(res_value) < int(min_value):
                         err.append('minValue')
-                    if max_value and int(res_value)>int(max_value):
+                    if max_value and int(res_value) > int(max_value):
                         err.append('maxValue')
                     if regex_pattern:
                         pass
@@ -256,9 +300,9 @@ def validate_expected_outcome(teststep, response):
                     if len(err):
                         is_failed = True
                         error = f"Validations not matched: {err}"
-        
+
         outcome.append({
-            **field, 
+            **field,
             "res_value": res_value,
             "executed_status": "failed" if is_failed else "passed",
             "status": "failed" if is_failed else "passed",
@@ -269,7 +313,7 @@ def validate_expected_outcome(teststep, response):
             no_of_failed_fields += 1
         else:
             no_of_passed_fields += 1
-    
-    if no_of_failed_fields==0:
+
+    if no_of_failed_fields == 0:
         return "passed", outcome, no_of_passed_fields, no_of_failed_fields
     return "failed", outcome, no_of_passed_fields, no_of_failed_fields
